cb0a5c0a0e14fadc11ddfb378c6091d17e1b4819
Beta brandao <roberta@robertabrandao.com.br>
2023-12-01 21:55:56 -0300

    add confs

---
+++ installs.sh
@@ -0,0 +1,4 @@
+sudo apt install xorg xinit
+sudo apt install git make gcc autoconf
+sudo apt install libpango1.0-dev libyajl-dev libstartup-notification0-dev libev-dev libtool libxkbcommon-dev libxkbcommon-x11-dev libxcb1-dev libxcb-randr0-dev libxcb-util0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxcb-shape0-dev libxcb-xrm-dev xutils-dev
+sudo apt install xrdp
