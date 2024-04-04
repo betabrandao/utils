f1baa97132b8ccec52d219574244c7b5136d270b
Beta Brandao <roberta@robertabrandao.com.br>
2024-04-04 19:43:00 -0300

    add telnet-chat projetc

---
+++ telnet-chat/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"config"  
+	"server"
+)
+
+func init() {
+	configFile := flag.String("config", "etc/config.json", "configuration file to parse")
+	flag.Parse()
+
+	log.Println("Reading configuration file:", *configFile)
+	if err := config.ReadConfiguration(*configFile); err != nil {
+		log.Fatalf("Failed to parse configuration file %q: %s", *configFile, err.Error())
+	}
+}
+
+func main() {
+	log.Println("Starting telnet chat server")
+
+	s, err := server.NewServer()
+	if err != nil {
+		log.Fatalf("Failed to initialize TCP listener: %s", err.Error())
+	}
+
+	s.Serve()
+}
